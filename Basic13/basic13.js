90a93da850a8d6ed34695d5db7df637ad6828170
Seth Green <sethezgreen@gmail.com>
2023-10-05 22:59:48 -0400

    added zero out method

--- Basic13/basic13.js
+++ Basic13/basic13.js
@@ -114,3 +114,16 @@ function squareValues(myArray) {
 
 myArray = [4,5,2,1,2,3,6]
 squareValues(myArray)
+
+// 12. Zero Out Negative Numbers
+function zeroOutNegativeNumbers(myArray) {
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] < 0) {
+            myArray[i] = 0
+        }
+    }
+    console.log(myArray)
+}
+
+myArray = [2,-4,5,-29,7]
+zeroOutNegativeNumbers(myArray)
